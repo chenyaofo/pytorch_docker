3f9f113f475ce1900c45d150da5777bc31a7d275
chenyaofo <chenyaofo@gmail.com>
2021-06-02 22:00:53 +0800

    fix.

--- render.py
+++ render.py
@@ -2,7 +2,7 @@ import os
 from jinja2 import Template
 
 cuda_versions = ['10.2', '11.3.0']
-miniconda_version = 'py37_4.9.2'
+miniconda_version = 'py38_4.9.2'
 py_version, *_ = miniconda_version.split("_")
 
 torch_base_version = "1.8.1"
@@ -40,7 +40,7 @@ with open(".github/workflows/dockerimage.yaml", "w") as workflows:
                 ]),
                 torch_version=torch_base_version + '+' + ('cu102' if cuda_version == "10.2" else 'cu111'),
                 torchvision_version='0.9.1' + '+' + ('cu102' if cuda_version == "10.2" else 'cu111'),
-                dali_package=None if not use_dali else 'nvidia-dali-cu100' if cuda_version == "10.2" else 'nvidia-dali-cu110'
+                dali_package=None if not use_dali else 'nvidia-dali-cuda100' if cuda_version == "10.2" else 'nvidia-dali-cuda110'
             )
             rendered_content = Template(open('Dockerfile.template').read(), trim_blocks=True).render(**build_vars)
 
